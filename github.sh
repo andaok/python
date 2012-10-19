75b396fc49121dcad091f0100a4ec701e30697d7
root <root@test1.terry.com>
2012-10-20 06:13:42 +0800

    github.sh for easy to manage github

---
+++ github.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#############################
+#THIS SCRIPT FOR EASY TO MANAGE GITHUB
+#############################
+
+PROJECTDIR="/var/data/github/python"
+GITHUBREPO="andaok/python.git"
+
+#SWITCH TO PROJECT DIR
+cd $PROJECTDIR
+
+CMD=$1
+COMMIT=$2
+DATE=$(date "+%Y%m%d/%H%M")
+
+if [ "X$CMD" == "Xadd" ]; then
+{
+   echo "START ADD AND UPDATE DATA TO GITHUB IN $DATE"
+   echo "-----------------------"
+   git add $PROJECTDIR 
+   git commit -m $COMMIT $PROJECTDIR 
+   git remote add origin git@github.com:$GITHUBREPO
+   echo "-----------------------"
+} >> /tmp/github.log 2>&1
+fi
+
+if [ "X$CMD" == "Xupdate" ]; then
+{
+   echo "START UPDATE DATA TO GITHUB IN $DATE"
+   echo "-----------------------"
+   git commit -m $COMMIT $PROJECTDIR
+   git push -u origin master
+   echo "-----------------------"
+} >> /tmp/github.log 2>&1
+fi
