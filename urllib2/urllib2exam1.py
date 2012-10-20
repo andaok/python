6c5832c33b2dc21b7785fd727565e8aed7828077
root <root@c02.cloudiya.com>
2012-10-20 15:35:01 +0800

    auto update data in cloudiya

--- urllib2/urllib2exam1.py
+++ urllib2/urllib2exam1.py
@@ -10,17 +10,18 @@ Created on Jun 9, 2012
 '''
 import urllib2
 
+#OPEN DEBUG MODEL
 httpHandler = urllib2.HTTPHandler(debuglevel=1)
 httpsHandler = urllib2.HTTPHandler(debuglevel=1)
-
 opener = urllib2.build_opener(httpHandler,httpsHandler)
-
 urllib2.install_opener(opener)
 
-response = urllib2.urlopen("http://192.168.0.111/video1.mp4")
+try:
+    response = urllib2.urlopen("http://192.168.0.111/video1.mp4")
+    print response.geturl()
+    print response.getcode()
+    print response.info()
+except urllib2.HTTPError,e:
+    print e.code
 
-help(response)
 
-print response.geturl()
-print response.getcode()
-print response.info()
