57fa1e0ad3568add612da9645d8607c0f4bbccbd
root <root@test1.terry.com>
2012-10-21 20:00:01 +0800

    just add or update data

--- mod_urllib2/urllib2exam2.py
+++ mod_urllib2/urllib2exam2.py
@@ -12,8 +12,19 @@ Send a data-stream to the stdin of a CGI and reading the data it returns to us
 
 import urllib2
 
+#OPEN DEBUG MODEL
+httpHandler = urllib2.HTTPHandler(debuglevel=1)
+httpsHandler = urllib2.HTTPHandler(debuglevel=1)
+opener = urllib2.build_opener(httpHandler,httpsHandler)
+urllib2.install_opener(opener)
+
 req = urllib2.Request(url='http://10.1.1.10/cgi-bin/test.cgi',data='this data is passed to stdin of the CGI')
 f = urllib2.urlopen(req)
 
 print f.read()
 
+#CGI PROGRAM
+#!/usr/bin/env python
+#import sys
+#data = sys.stdin.read()
+#print 'Content-type: text-plain\n\nGot Data: "%s"' % data
