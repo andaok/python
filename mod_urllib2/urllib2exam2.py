a127908acc2d4cd6ddad24ae33815ecf651aa129
root <root@test1.terry.com>
2012-10-21 19:25:01 +0800

    just add or update data

--- mod_urllib2/urllib2exam2.py
+++ mod_urllib2/urllib2exam2.py
@@ -5,3 +5,15 @@ Created on 2012-10-21
 
 @author: root
 '''
+
+"""
+Send a data-stream to the stdin of a CGI and reading the data it returns to us
+"""
+
+import urllib2
+
+req = urllib2.Request(url='http://10.1.1.10/test.cgi',data='this data is passed to stdin of the CGI')
+f = urllib2.urlopen(req)
+
+print f.read()
+
