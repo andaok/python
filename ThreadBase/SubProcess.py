2ed841e2af1881cda2a66aff37e2eee82025a8ca
root <root@test1.terry.com>
2012-10-26 07:30:01 +0800

    just add or update data

---
+++ ThreadBase/SubProcess.py
@@ -0,0 +1,27 @@
+'''
+Created on 2012-10-26
+
+@author: root
+'''
+
+from subprocess import * 
+import time
+
+timeout = 1
+list = {}
+
+for i in range(4):
+    p = Popen("cp /tmp/test/tmp%s.txt  /tmp/test/temp%s.txt"%(i+1,i+1),shell=True)
+    list[i] = p
+    
+print list 
+
+for i in list:
+    done = False
+    while not done or timeout > 0:
+        time.sleep(0.2)
+        if list[i].wait() != -1:
+            done = True
+        timeout -=0.2
+         
+    
