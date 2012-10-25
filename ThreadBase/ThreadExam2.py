4c720f5f3465b1d1ae496daa0372611f9ce775f9
root <root@test1.terry.com>
2012-10-26 06:55:01 +0800

    just add or update data

---
+++ ThreadBase/ThreadExam2.py
@@ -0,0 +1,21 @@
+'''
+Created on 2012-10-26
+
+@author: root
+'''
+
+import threading
+import time
+
+count = 1
+
+class KissThread(threading.Thread):
+    def run(self):
+        global count
+        print "Thread # %s: Pretending to do stuff" % count
+        count +=1
+        time.sleep(2)
+        print "done with stuff"
+
+for t in range(5):
+    KissThread().start()
