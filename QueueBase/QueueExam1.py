82b3b4a12581ee84b322b0378cf6195cbcac6ec4
root <root@test1.terry.com>
2012-10-28 19:55:01 +0800

    just add or update data

---
+++ QueueBase/QueueExam1.py
@@ -0,0 +1,36 @@
+'''
+Created on 2012-10-28
+
+@author: root
+'''
+
+import threading
+import Queue
+import time,random
+
+WORKERS = 2
+
+class Worker(threading.Thread):
+    
+    def __init__(self,queue):
+        self.__queue = queue
+        threading.Thread.__init__(self)
+    
+    def run(self):
+        while 1:
+            item = self.__queue.get()
+            if item is None:
+                break
+            time.sleep(random.randint(10,100)/1000)
+            print "task" , item ,"finished"
+
+queue = Queue.Queue(0)
+
+for i in range(WORKERS):
+    Worker(queue).start()
+    
+for i in range(10):
+    queue.put(i)
+
+for i in range(WORKERS):
+    queue.put(None)
