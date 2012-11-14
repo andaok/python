5cc400dd66593a2af55aeedf6dc8d41afe56d293
root <root@test1.terry.com>
2012-11-15 05:50:01 +0800

    just add or update data

--- ReadBookNote/LinuxSysAdmin/2_Text.py
+++ ReadBookNote/LinuxSysAdmin/2_Text.py
@@ -21,3 +21,11 @@ print """ jack
 uname = "Linux #1 SMP Tue Feb"
 
 print "Linux" in uname
+print "Linux" not in uname
+
+print uname.find("SMP")
+print uname.index("SMP")
+
+SMP_index = uname.index("SMP")
+print uname[SMP_index:]
+print uname[:SMP_index]
