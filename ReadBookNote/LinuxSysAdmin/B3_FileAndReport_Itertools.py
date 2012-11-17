07c6f133979e81d24d55e1b6a5b8042e4bcd8673
root <root@c02.cloudiya.com>
2012-11-17 17:00:01 +0800

    auto update data in cloudiya

--- ReadBookNote/LinuxSysAdmin/B3_FileAndReport_Itertools.py
+++ ReadBookNote/LinuxSysAdmin/B3_FileAndReport_Itertools.py
@@ -4,12 +4,21 @@
 itertools - Functional tools for creating and using iterators.
 """
 
-import itertools
+from itertools import *
 
 #chain将多个iterator合并成一个iterator
-for i in itertools.chain([1,2,3],['a','b','c']):
+for i in chain([1,2,3],['a','b','c']):
     print i
     
-#izip
-for i in itertools.izip([1,2,3],['a','b','c']):
+#izip将多个iterator迭代的内容混合成一个元组。
+for i in izip([1,2,3],['a','b','c']):
+    print i
+
+#count
+#count([n]) --> n, n+1, n+2, ...
+
+#islice
+# islice(seq, [start,] stop [, step]) --> elements from seq[start:stop:step]
+print "by tens to 100"
+for i in islice(count(),0,100,10):
     print i
