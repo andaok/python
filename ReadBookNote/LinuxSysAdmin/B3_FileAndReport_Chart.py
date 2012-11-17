59b2afc1f62f5c2ce2256bd72cf71df139666e74
root <root@test1.terry.com>
2012-11-18 06:45:02 +0800

    just add or update data

--- ReadBookNote/LinuxSysAdmin/B3_FileAndReport_Chart.py
+++ ReadBookNote/LinuxSysAdmin/B3_FileAndReport_Chart.py
@@ -3,25 +3,28 @@
 import gdchart
 import shelve
 
-shelve_file = shelve.open("File/BytesSumPerHost.bat")
+def chartBar():
+    shelve_file = shelve.open("File/BytesSumPerHost.bat")
 
-#元组列表
-items_list = [(i[1],i[0]) for i in shelve_file.items()]
-items_list.sort()
+    #元组列表
+    items_list = [(i[1],i[0]) for i in shelve_file.items()]
+    items_list.sort()
 
-bytes_sent = [i[0] for i in items_list]
-ip_addresses = [i[1] for i in items_list]
+    bytes_sent = [i[0] for i in items_list]
+    ip_addresses = [i[1] for i in items_list]
 
-chart = gdchart.Bar() 
-chart.width = 400
-chart.height = 400
-chart.bg_color = "white"
-chart.plot_color = "black"
-chart.xtitle = "IP ADDRESS"
-chart.ytitle = "Bytes Sent"
-chart.title = "Usage bytes by ip address"
-chart.setData(bytes_sent)
-chart.setLabels(ip_addresses)
-chart.draw("File/bytes_ip_adr.png")
+    chart = gdchart.Bar() 
+    chart.width = 400
+    chart.height = 400
+    chart.bg_color = "white"
+    chart.plot_color = "black"
+    chart.xtitle = "IP ADDRESS"
+    chart.ytitle = "Bytes Sent"
+    chart.title = "Usage bytes by ip address"
+    chart.setData(bytes_sent)
+    chart.setLabels(ip_addresses)
+    chart.draw("File/bytes_ip_adr.png")
+    shelve_file.close()
 
-shelve_file.close()
+chartBar()
+
