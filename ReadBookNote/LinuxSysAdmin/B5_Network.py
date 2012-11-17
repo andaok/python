ddda7441b9f55e914d54dde5c3a9a4cba4e6705a
root <root@test1.terry.com>
2012-11-18 07:50:01 +0800

    just add or update data

---
+++ ReadBookNote/LinuxSysAdmin/B5_Network.py
@@ -0,0 +1,14 @@
+#-*- encoding -*-
+
+'''
+Created on 2012-11-18
+
+@author: root
+'''
+
+#------splitlines----------------------
+str1 = "hello world,mysql world \n ni hao,mysql world \n bejing ,welcome to you"
+
+print str1.splitlines()
+
+#--------------------------------------
