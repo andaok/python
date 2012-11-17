d6e5d690f71cfa23c3d3aa49eb1751474ec9e7d6
root <root@test1.terry.com>
2012-11-18 07:05:01 +0800

    just add or update data

--- ReadBookNote/LinuxSysAdmin/B3_FileAndReport_Chart.py
+++ ReadBookNote/LinuxSysAdmin/B3_FileAndReport_Chart.py
@@ -35,8 +35,9 @@ def chartPie():
     items_list = [(i[1],i[0]) for i in shelve_file.items()]
     items_list.sort()
     bytes_sent = [i[0] for i in items_list]
+    print len(bytes_sent)
     ip_addresses = [i[1] for i in items_list]
-    
+    print len(ip_addresses)
     chart = gdchart.Pie()
     chart.width = 800
     chart.height = 800
