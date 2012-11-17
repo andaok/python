b0b3064646c79f92926fd8f3b932eb74cfd5222e
root <root@test1.terry.com>
2012-11-18 07:00:01 +0800

    just add or update data

--- ReadBookNote/LinuxSysAdmin/B3_FileAndReport_Chart.py
+++ ReadBookNote/LinuxSysAdmin/B3_FileAndReport_Chart.py
@@ -26,24 +26,37 @@ def chartBar():
     chart.draw("File/bytes_ip_adr.png")
     shelve_file.close()
 
-chartBar()
+#chartBar()
 
 import itertools
 def chartPie():
     shelve_file = shelve.open("File/BytesSumPerHost.bat")
-    items_list = [(i[1],i[0]) for i in shelve.items() if i[1]>0]
+    #items_list = [(i[1],i[0]) for i in shelve_file.items() if i[1]>0]
+    items_list = [(i[1],i[0]) for i in shelve_file.items()]
     items_list.sort()
     bytes_sent = [i[0] for i in items_list]
     ip_addresses = [i[1] for i in items_list]
     
-    chart = gdchart.pie()
+    chart = gdchart.Pie()
     chart.width = 800
     chart.height = 800
     chart.bg_color = 'white'
     color_cycle = itertools.cycle([0xDDDDDD,0x111111,0x777777])
     color_list = []
     for i in bytes_sent:
-        color_list.append()
+        color_list.append(color_cycle.next())
+    chart.color = color_list
+    chart.plot_color = 'black'
+    chart.title = "Usage By Ip Address"
+    chart.setData(bytes_sent)
+    chart.setLabels(ip_addresses)
+    chart.draw("File/bytes_ip_pie.png")
+
+    shelve_file.close()    
+    
+chartPie()    
+   
+    
     
     
     
