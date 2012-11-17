288ded8d886891d7b1aa91ee7383ef68545a6a67
root <root@test1.terry.com>
2012-11-18 07:10:01 +0800

    just add or update data

--- ReadBookNote/LinuxSysAdmin/B3_FileAndReport_Chart.py
+++ ReadBookNote/LinuxSysAdmin/B3_FileAndReport_Chart.py
@@ -35,12 +35,12 @@ def chartPie():
     items_list = [(i[1],i[0]) for i in shelve_file.items()]
     items_list.sort()
     bytes_sent = [i[0] for i in items_list]
-    print len(bytes_sent)
+    print bytes_sent
     ip_addresses = [i[1] for i in items_list]
-    print len(ip_addresses)
+    print ip_addresses
     chart = gdchart.Pie()
-    chart.width = 800
-    chart.height = 800
+    chart.width = 400
+    chart.height = 400
     chart.bg_color = 'white'
     color_cycle = itertools.cycle([0xDDDDDD,0x111111,0x777777])
     color_list = []
@@ -49,7 +49,7 @@ def chartPie():
     chart.color = color_list
     chart.plot_color = 'black'
     chart.title = "Usage By Ip Address"
-    chart.setData(bytes_sent)
+    chart.setData(12, 12, 24, 48, 140)
     chart.setLabels(ip_addresses)
     chart.draw("File/bytes_ip_pie.png")
 
