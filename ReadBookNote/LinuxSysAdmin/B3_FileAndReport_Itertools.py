b6a50b136bbe14589c3d1df73427a6951ec4b73c
root <root@c02.cloudiya.com>
2012-11-17 18:00:01 +0800

    auto update data in cloudiya

--- ReadBookNote/LinuxSysAdmin/B3_FileAndReport_Itertools.py
+++ ReadBookNote/LinuxSysAdmin/B3_FileAndReport_Itertools.py
@@ -10,6 +10,12 @@ from itertools import *
 for i in chain([1,2,3],['a','b','c']):
     print i
     
+#-------chain.from_iterable(iterables)--
+test = chain.from_iterable("abcdefg")
+print test.next()
+print test.next()    
+
+    
 #izip将多个iterator迭代的内容混合成一个元组。
 for i in izip([1,2,3],['a','b','c']):
     print i
@@ -17,8 +23,35 @@ for i in izip([1,2,3],['a','b','c']):
 #count
 #count([n]) --> n, n+1, n+2, ...
 
-#islice
-# islice(seq, [start,] stop [, step]) --> elements from seq[start:stop:step]
+
+#---------islice-----------------------
+
+#islice(iterator, [start,] stop [, step]) --> elements from iterator[start:stop:step]
+#islice类似于切片,把iterator按照规则分切返回一个迭代器
+str = ""
+for i in islice("abcdefg",0,4,1):
+    str = str + i +","
+print str.strip(",")   # a,b,c,d
+
+str = ""
+for i in islice("abcdefg",2,5,2):
+    str = str + i +","
+print str.strip(",")   # c,e
+
+#count()为一迭代器 => 0,1,2,3.......
 print "by tens to 100"
+str=""
 for i in islice(count(),0,100,10):
-    print i
+    str = str + "%s"%i +","
+print str.strip(",")   # 0,10,20,30,40,50,60,70,80,90
+    
+#-----------------------------------------
+
+   
+    
+    
+    
+    
+    
+    
+    
