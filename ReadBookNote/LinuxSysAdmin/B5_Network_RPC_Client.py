50ae157161c9599ca68fbe5432601c082f449619
root <root@test1.terry.com>
2012-11-18 20:25:01 +0800

    just add or update data

--- ReadBookNote/LinuxSysAdmin/B5_Network_RPC_Client.py
+++ ReadBookNote/LinuxSysAdmin/B5_Network_RPC_Client.py
@@ -8,12 +8,15 @@ Created on 2012-11-18
 
 import xmlrpclib
 
-x = xmlrpclib.ServerProxy('http://127.0.0.1:8765')
+def XMLRPC():
+    x = xmlrpclib.ServerProxy('http://127.0.0.1:8765')
 
-print x.ls('.')
+    print x.ls('.')
 
-print x.ls('/tmp/')
+    print x.ls('/tmp/')
 
-print x.ls('/tmp/nodir')
+    print x.ls('/tmp/nodir')
 
-print x.ls_boom('/tmp/nopdir')
+    print x.ls_boom('/tmp/nopdir')
+
+
