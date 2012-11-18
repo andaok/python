9d5df8effe034d083c6d86850d25af991af0af76
root <root@test1.terry.com>
2012-11-18 20:00:01 +0800

    just add or update data

---
+++ ReadBookNote/LinuxSysAdmin/B5_Network_RPC_Client.py
@@ -0,0 +1,7 @@
+#-*- encoding:utf-8 -*-
+
+'''
+Created on 2012-11-18
+
+@author: root
+'''
---
+++ ReadBookNote/LinuxSysAdmin/B5_Network_RPC_Server.py
@@ -0,0 +1,33 @@
+#-*- encoding:utf-8 -*-
+
+'''
+Created on 2012-11-18
+
+@author: root
+'''
+
+import SimpleXMLRPCServer
+import os
+
+def ls(directory):
+    try:
+        return os.listdir(directory)
+    except OSError:
+        return []
+
+def ls_boom(directory):
+    return os.listdir(directory)
+
+def cb(obj):
+    print "OBJECT::",obj
+    print "OBJECT.__class__",obj.__class__
+    return obj.cb()
+
+if __name__ == "__main__":
+    s = SimpleXMLRPCServer.SimpleXMLRPCServer(('127.0.0.1',8765))
+    s.register_function(ls)
+    s.register_function(ls_boom)
+    s.register_function(cb)
+    s.serve_forever()
+    
+
