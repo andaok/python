2634bd2b1d5b0918ff79c4245187529fc997d5cf
root <root@c02.cloudiya.com>
2012-11-20 12:30:01 +0800

    auto update data in cloudiya

--- OOPBase/OOP_Exam2.py
+++ OOPBase/OOP_Exam2.py
@@ -4,12 +4,14 @@ class Person:
         self.name = name
         self.age = age
         Person.PersonNum +=1
+
     def tell(self):
         print "this person information name %s,age %s"%(self.name,self.age)
     def currentPerNum(self):
         print "Current person number is %s"%(Person.PersonNum)
     def __del__(self):
         Person.PersonNum -=1
+
 
 class Student(Person):
     def __init__(self,name,age,mark):
@@ -18,6 +20,8 @@ class Student(Person):
     def tell(self):
         Person.tell(self)
         print "student mark is %s"%(self.mark)
+    def __del__(self):
+        Person.__del__(self)
 
 s1 = Student("hack","26","67")
 s1.tell()
