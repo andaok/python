7dd41ef24895742f2b04cdc843eb718ebb1c54ae
root <root@c02.cloudiya.com>
2012-11-20 14:30:01 +0800

    auto update data in cloudiya

--- OOPBase/OOP_Exam2.py
+++ OOPBase/OOP_Exam2.py
@@ -25,16 +25,16 @@ class Student(Person):
         print "student mark is %s"%(self.mark)
 
 
-z1 = Student("hack","26","67")
-z1.tell()
-z1.currentPerNum()
+_z1 = Student("hack","26","67")
+_z1.tell()
+_z1.currentPerNum()
 
-z2 = Student("kill","27","76")
+_z2 = Student("kill","27","76")
 
-z3 = Student("woji","26","87")
-z3.tell()
-z3.currentPerNum()
+_z3 = Student("woji","26","87")
+_z3.tell()
+_z3.currentPerNum()
 
-del z2
-z1.currentPerNum()
-z3.currentPerNum()
+del _z2
+_z1.currentPerNum()
+_z3.currentPerNum()
