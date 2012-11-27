7e169f33e0f739524489b761f42b4ad7d6ae1b1f
root <root@c02.cloudiya.com>
2012-11-27 11:00:01 +0800

    auto update data in cloudiya

--- ReadBookNote/LinuxSysAdmin/B4_FileAndReport_Chart.py
+++ ReadBookNote/LinuxSysAdmin/B4_FileAndReport_Chart.py
@@ -23,10 +23,10 @@ def chartBar():
     chart.title = "Usage bytes by ip address"
     chart.setData(bytes_sent)
     chart.setLabels(ip_addresses)
-    chart.draw("File/bytes_ip_adr.png")
+    chart.draw("File/bytes_ip_bar.png")
     shelve_file.close()
 
-chartBar()
+#chartBar()
 
 import itertools
 def chartPie():
@@ -54,10 +54,32 @@ def chartPie():
 
     shelve_file.close()    
     
-chartPie()    
+#chartPie()    
    
-    
-    
+def chartLine():
+    shelve_file = shelve.open("File/BytesSumPerHost.bat")
+
+    #元组列表
+    items_list = [(i[1],i[0]) for i in shelve_file.items()]
+    items_list.sort()
+
+    bytes_sent = [i[0] for i in items_list]
+    ip_addresses = [i[1] for i in items_list]
+
+    chart = gdchart.Line() 
+    chart.width = 400
+    chart.height = 400
+    chart.bg_color = "white"
+    chart.plot_color = "black"
+    chart.xtitle = "IP ADDRESS"
+    chart.ytitle = "Bytes Sent"
+    chart.title = "Usage bytes by ip address"
+    chart.setData(bytes_sent)
+    chart.setLabels(ip_addresses)
+    chart.draw("File/bytes_ip_line.png")
+    shelve_file.close()    
+
+chartLine() 
     
     
     
