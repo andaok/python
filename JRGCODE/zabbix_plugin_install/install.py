ccb74e8f285a0717d9fce3f8328592f546403ded
andaok <ywok163@gmail.com>
2016-12-16 20:09:45 +0800

    no log

---
+++ JRGCODE/zabbix_plugin_install/install.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# --------------------------------------------
+# @Date    : 2016-11-30 11:51:27
+# @Author  : wye
+# @Version : v1.0
+# @Descr   : zabbix plugin install script
+# --------------------------------------------
+
+
